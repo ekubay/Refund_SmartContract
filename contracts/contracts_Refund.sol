44907215e4254c7c0a61fb5610908efc64fc80c6
ekubay <axutec14@gmail.com>
2022-10-29 10:00:13 +0300

    contracts

--- contracts/contracts_Refund.sol
+++ contracts/contracts_Refund.sol
@@ -3,71 +3,130 @@
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
-
-// import "hardhat/console.sol";
+// import "@openzeppelin/contracts/utils/Strings.sol";
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
-contract Token {
-    // Some string type variables to identify the token.
-    string public name = "My Hardhat Token";
-    string public symbol = "MHT";
+contract Refund {
+    // Model a Candidate
 
-    // The fixed amount of tokens, stored in an unsigned integer type variable.
-    uint256 public totalSupply = 1000000;
+    event CompletedEvent (
+        address employeeaddress
+    );
 
-    // An address type variable is used to store ethereum accounts.
-    address public owner;
-
-    // A mapping is a key/value map. Here we store each account's balance.
-    mapping(address => uint256) balances;
-
-    // The Transfer event helps off-chain applications understand
-    // what happens within your contract.
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
-    /**
-     * Contract initialization.
-     */
-    constructor() {
-        // The totalSupply is assigned to the transaction sender, which is the
-        // account that is deploying the contract.
-        balances[msg.sender] = totalSupply;
-        owner = msg.sender;
+    event FailedEvent (
+        address employeeaddress
+    );
+    // employee data
+    struct Employee {
+        uint id;
+        string name;
+        address public_address;
     }
 
-    /**
-     * A function to transfer tokens.
-     *
-     * The `external` modifier makes a function *only* callable from *outside*
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-    //     console.log(
-    //     "Transferring from %s to %s %s tokens",
-    //     msg.sender,
-    //     to,
-    //     amount
-    // );
-
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-
-        // Notify off-chain applications of the transfer.
-        emit Transfer(msg.sender, to, amount);
+    //employer data
+    struct Employer {
+        uint id;
+        string name;
+        address public_address;
     }
 
-    /**
-     * Read only function to retrieve the token balance of a given account.
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return balances[account];
+    //contract data with boundary points , durations , completion checkers
+    struct contract_data{
+        uint id;
+        uint minimum_point_long;
+        uint minimum_point_lat;
+        uint maximum_point_long;
+        uint maximum_point_lat;
+        uint starting_time;
+        uint duration;
+        uint gathered_location_count;
+        bool contract_truth;
+        bool completed;
+        // creating struct instances 
+        Employee employees;
+        Employer employer;
     }
+    
+    // contracts that have been recorded 
+    mapping(address => contract_data) public contracts;
+    
+    uint public Contractcount;
+
+    // Employees that have been recorded
+    mapping(address => Employee) public employees;
+    
+    mapping (uint => address) public employee_mapping;
+ 
+    uint public Employeecount;
+
+    // employer that have been recorded
+    mapping(address => Employer) public employers;
+    uint public Employercount;
+
+    function  initialize_employers(address[] memory a) public {
+        for (uint add=0 ; add < a.length; add++){
+            addEmployer(string.concat("Employer " , Strings.toString(add)) , a[add]);
+            }
+        // addEmployee("Employee 1" , a[1]);
+    }
+    function  initialize_employees(address[] memory a) public{
+        // addEmployer("Employer 1" , a[0]);
+         for (uint add=0 ; add < a.length; add++){
+            addEmployee(string.concat("Employee " , Strings.toString(add)) , a[add]);
+            }
+    }
+
+    function addEmployer (string memory _name , address user_address) private {
+        Employercount ++;
+        employers[user_address] = Employer(Employercount, _name, user_address);
+    }
+    function addEmployee (string memory _name , address user_address) private {
+        Employeecount ++;
+        employees[user_address] = Employee(Employeecount, _name, user_address);
+    }
+
+    function Create_contract_data( uint[2] memory minimum_points, uint[2] memory maximum_points, uint duration, string memory employee_name , address employee_address, address employer_address) public{
+        if (! (employees[employee_address].id > 0)){
+            addEmployee(employee_name, employee_address);
+            employee_mapping[Employeecount] = employee_address;
+         }
+        Contractcount++;
+        contracts[employee_address] = contract_data(Contractcount, minimum_points[0] , minimum_points[1] ,maximum_points[0] , maximum_points [1], block.timestamp ,duration , 0 ,true , false ,employees[employee_address],employers[employer_address]);
+    }
+
+    function get_location( uint  longitude, uint  latitude) public  returns (bool){
+        
+        contract_data memory found_contract = contracts[msg.sender];
+        if (found_contract.id > 0){
+        if ( found_contract.completed != true){
+            uint duration = (block.timestamp - found_contract.starting_time) / 60 ;
+            if (duration < found_contract.duration){ 
+                found_contract.gathered_location_count = found_contract.gathered_location_count + 1;
+                if (! (found_contract.minimum_point_long <= longitude && found_contract.maximum_point_long >= longitude && found_contract.minimum_point_lat <= latitude && found_contract.maximum_point_lat >= latitude) ){
+                    found_contract.contract_truth = false;
+                }
+            }else if ( duration >=found_contract.duration){
+                check_completion(found_contract);
+               
+            }
+        }
+            return true;
+            }else{
+                return false;
+            }
+    }
+
+    function check_completion(contract_data memory found_contract) private{
+        uint minimum_check = found_contract.duration * 3 / 4 ;
+        if (found_contract.contract_truth && minimum_check <= found_contract.gathered_location_count){
+                    found_contract.completed = true;
+                    emit CompletedEvent(found_contract.employees.public_address);
+                }
+                else{
+                    found_contract.completed = false;
+                    emit FailedEvent(found_contract.employees.public_address);
+                }
+    }
+
 }
