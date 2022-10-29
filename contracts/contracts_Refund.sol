e94f190ada721ed249ce1d2090859fbd083e6ec6
ekubay <axutec14@gmail.com>
2022-10-29 11:28:44 +0300

    creating contracts

--- contracts/contracts_Refund.sol
+++ contracts/contracts_Refund.sol
@@ -20,24 +20,22 @@ contract Refund {
     // employee data
     struct Employee {
         uint id;
-        string name;
-        address public_address;
+        string employeename;
+        address employeeaddress;
     }
 
     //employer data
     struct Employer {
         uint id;
-        string name;
-        address public_address;
+        string employername;
+        address employeraddress;
     }
 
     //contract data with boundary points , durations , completion checkers
     struct contract_data{
         uint id;
-        uint minimum_point_long;
-        uint minimum_point_lat;
-        uint maximum_point_long;
-        uint maximum_point_lat;
+        uint employeelatitude;
+        uint employeelonguited;
         uint starting_time;
         uint duration;
         uint gathered_location_count;
@@ -64,16 +62,16 @@ contract Refund {
     mapping(address => Employer) public employers;
     uint public Employercount;
 
-    function  initialize_employers(address[] memory a) public {
-        for (uint add=0 ; add < a.length; add++){
-            addEmployer(string.concat("Employer " , Strings.toString(add)) , a[add]);
+            // adding employer
+    function  initialize_employers(address[] memory m) public {
+        for (uint add=0 ; add < m.length; add++){
+            addEmployer(string.concat("Employer " , string.toString(add)) , m[add]);
             }
-        // addEmployee("Employee 1" , a[1]);
     }
-    function  initialize_employees(address[] memory a) public{
-        // addEmployer("Employer 1" , a[0]);
-         for (uint add=0 ; add < a.length; add++){
-            addEmployee(string.concat("Employee " , Strings.toString(add)) , a[add]);
+            // adding employee
+   function  initialize_employees(address[] memory m) public{
+         for (uint add=0 ; add < m.length; add++){
+            addEmployee(string.concat("Employee " , string.toString(add)) , m[add]);
             }
     }
 
@@ -86,13 +84,13 @@ contract Refund {
         employees[user_address] = Employee(Employeecount, _name, user_address);
     }
 
-    function Create_contract_data( uint[2] memory minimum_points, uint[2] memory maximum_points, uint duration, string memory employee_name , address employee_address, address employer_address) public{
+    function Create_contract_data( uint[2] memory employeelatitude, uint[2] memory employeelonguited, uint duration, string memory employee_name , address employee_address, address employer_address) public{
         if (! (employees[employee_address].id > 0)){
             addEmployee(employee_name, employee_address);
             employee_mapping[Employeecount] = employee_address;
          }
         Contractcount++;
-        contracts[employee_address] = contract_data(Contractcount, minimum_points[0] , minimum_points[1] ,maximum_points[0] , maximum_points [1], block.timestamp ,duration , 0 ,true , false ,employees[employee_address],employers[employer_address]);
+        contracts[employee_address] = contract_data(Contractcount, employeelatitude[0] , employeelonguited[1], block.timestamp ,duration , 0 ,true , false ,employees[employee_address],employers[employer_address]);
     }
 
     function get_location( uint  longitude, uint  latitude) public  returns (bool){
@@ -103,7 +101,7 @@ contract Refund {
             uint duration = (block.timestamp - found_contract.starting_time) / 60 ;
             if (duration < found_contract.duration){ 
                 found_contract.gathered_location_count = found_contract.gathered_location_count + 1;
-                if (! (found_contract.minimum_point_long <= longitude && found_contract.maximum_point_long >= longitude && found_contract.minimum_point_lat <= latitude && found_contract.maximum_point_lat >= latitude) ){
+                if (! (found_contract.employeelonguited <= longitude && found_contract.employeelatitude <= latitude) ){
                     found_contract.contract_truth = false;
                 }
             }else if ( duration >=found_contract.duration){
@@ -121,11 +119,11 @@ contract Refund {
         uint minimum_check = found_contract.duration * 3 / 4 ;
         if (found_contract.contract_truth && minimum_check <= found_contract.gathered_location_count){
                     found_contract.completed = true;
-                    emit CompletedEvent(found_contract.employees.public_address);
+                    emit CompletedEvent(found_contract.employees.employeraddress);
                 }
                 else{
                     found_contract.completed = false;
-                    emit FailedEvent(found_contract.employees.public_address);
+                    emit FailedEvent(found_contract.employees.employeraddress);
                 }
     }
 
